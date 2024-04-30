480b52d8fb2c02a3d071d56fc2118d3e02190b64
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-04-30 14:18:02 +0530

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import DB_NAME from "./constants";
+import {DB_NAME} from "./constants";
 (async () => {
   try {
     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
