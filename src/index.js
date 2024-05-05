c56274383f6582b12ff61ddd97f68a128f09aee0
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-05-05 17:42:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
+import { DB_NAME } from "./constants";
+import express from "express";
+require("dotenv").config();
+const app = express();
+
+
 (async () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
   } catch (error) {
     console.log("ERROR: ", error);
     throw error;
